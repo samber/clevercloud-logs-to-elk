8124bd9a02f804daab8e88ece4f47d508c831b99
Samuel Berthe <dev@samuel-berthe.fr>
2018-08-24 22:13:30 +0200

    fix

--- run.sh
+++ run.sh
@@ -11,5 +11,4 @@ PREFIX="${ORG_NAME}\\ ${CLEVERCLOUD_APP}\\ ${APP_NAME}\\ "
 exec clever logs --no-update-notifier=true --after=$(date --iso-8601=seconds) \
     | grep --line-buffered -v 'Waiting for application logs' \
     | sed -u "s#^#${PREFIX}#g" \
-    | tee -a /tmp/logs.txt \
     | nc ${LOGSTASH_HOST} ${LOGSTASH_PORT}
